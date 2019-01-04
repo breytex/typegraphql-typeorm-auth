3dd834047ec8c053383221013f76488c2ecb2879
Simon <simon@holobuilder.com>
2019-01-04 22:58:24 +0100

    formatting

--- backend/src/resolvers/User.ts
+++ backend/src/resolvers/User.ts
@@ -1,14 +1,14 @@
 import { Query, Arg, Mutation } from "type-graphql";
-import  { UserInput, User } from "../entity/User";
+import { UserInput, User } from "../entity/User";
 
-export class UserResolver{
+export class UserResolver {
   @Query(returns => User)
   async user(@Arg("id") id: string) {
-    return await User.findOne({id})
+    return await User.findOne({ id })
   }
-  
+
   @Mutation(returns => User)
-  async addUser(@Arg("user") { email }: UserInput){
+  async addUser(@Arg("user") { email }: UserInput) {
     const user = User.create({ email })
     await user.save()
     return user
